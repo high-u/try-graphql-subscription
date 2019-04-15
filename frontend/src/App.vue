3e277d2c679fe90c3f0448dfc303bad191c325f5
high-u <zen.crazyd@gmail.com>
2019-04-16 01:29:57 +0900

    Building the frontend app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container" style="padding-top: 100px">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-body">
+            <div class="row" v-if="entered">
+              <div class="col-md-12">
+                <div class="card">
+                  <div class="card-header">Chatbox</div>
+                  <div class="card-body">
+                    <!-- messages will be here -->
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row" v-else>
+              <div class="col-md-12">
+                <form method="post" @submit.prevent="enterChat">
+                  <div class="form-group">
+                    <div class='input-group'>
+                      <input
+                        type='text'
+                        class="form-control"
+                        placeholder="Enter your username"
+                        v-model="username"
+                      >
+                      <div class='input-group-append'>
+                        <button class='btn btn-primary' @click="enterChat">Enter</button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      username: '',
+      message: '',
+      entered: false,
+    };
+  },
+  methods: {
+    enterChat() {
+      this.entered = !!this.username != '';
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
